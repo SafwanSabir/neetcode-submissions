e8903ee8afd4a43835258b79f45ace2efdd32eab
SafwanSabir <142938551+SafwanSabir@users.noreply.github.com>
2026-09-04 18:20:31 +0500

    Refactor palindrome check logic
    Removed commented-out code for whitespace handling and improved character validation logic.

--- Data Structures & Algorithms/is-palindrome/submission-2.cpp
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -5,8 +5,6 @@ public:
         int start=0,end=s.length();
         end--;
         while(start<end){
-            // while(s[start]==' '){start++;}
-            // while(s[end]==' '){end--;}
             while(!(s[start]>=97 && s[start]<=122 || s[start]>=48 && s[start]<=57 || s[start]>=65 && s[start]<=90) && start<end){start++;}
             while(!(s[end]>=97 && s[end]<=122 || s[end]>=48 && s[end]<=57 || s[end]>=65 && s[end]<=90) && start<end){end--;}
             if(s[start]>=65 && s[start]<=90){s[start]=tolower(s[start]);}
